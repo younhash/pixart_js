995eed433ac9e0c6abcc8bf4dfec992aadf4584c
Mohammad Jaiteh <jaitehmohammad.com>
2019-09-05 18:07:21 -0400

    I now have the color button ready, it can change color background when clicked

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+let button = document.querySelector('#set-color');
+let brush = document.querySelector('.brush');
+
+button.addEventListener('click', setTheColor);
+
+function setTheColor(e) {
+    e.preventDefault();
+   inputValue = document.querySelector('#color-field');
+   brush.style.background = inputValue.value;  
+}
 
